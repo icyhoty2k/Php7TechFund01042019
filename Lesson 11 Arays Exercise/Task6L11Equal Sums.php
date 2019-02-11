80a34e7ccca07a7379a03758c1deace199c529d4
G750js <icyhoty2k@gmail.com>
2019-02-11 04:00:42 +0200

    first phpGitCommit TF2PHP2019

--- Lesson 11 Arays Exercise/Task6L11Equal Sums.php
+++ Lesson 11 Arays Exercise/Task6L11Equal Sums.php
@@ -10,3 +10,34 @@
 /**
  *
  */
+
+
+$input = readline();
+$sum1=0;
+$sum2=0;
+$x=0;
+$arr=explode(" ",$input);
+if(count($arr)<=1){
+    echo "0";
+    $x=1;
+
+}
+for($i=0;$i<count($arr)+$i;$i++){
+
+    $sum1+=$arr[0];
+array_shift($arr);
+if(!empty($arr)){
+    $sum2=array_sum($arr)-($arr[0]);
+}
+    if($sum2 == $sum1){
+        $x=1;
+        echo $i+1;
+            break;
+    }
+
+}
+if($x!=1){
+    echo "no";
+}
+
+

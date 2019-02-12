bb8b5df69e0a3f4c5428a3d834c75be7d8e57f29
G750js <icyhoty2k@gmail.com>
2019-02-12 02:21:15 +0200

    first phpGitCommit TF2PHP2019

---
+++ Lesson 11 Arays Exercise/Task7L117Max Sequence of Equal Elements.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: G750js
+ * Date: 11.2.2019 г.
+ * Time: 22:09
+ */
+
+
+/**
+ *Write a program that finds the longest sequence of equal elements in an array of integers. If several longest sequences exist, print the leftmost one.
+ */
+
+$input = readline();
+$arr = explode(" ", $input);
+$arrMatches=[];
+$size=count($arr);
+$currentMatches=[];
+for($i=0; $i<count($arr); $i++){
+    if($i==count($arr)-1 && empty($arrMatches)){
+       array_push($currentMatches ,$arr[0]);
+       array_push($arrMatches , $currentMatches);
+    }
+    $count;
+    $currEl=$arr[$i];
+        for($j=1+$i; $j<count($arr); $j++){
+            $nextEl = $arr[$j];
+        if($currEl==$nextEl){
+
+            $k=$j;
+
+
+            array_push($currentMatches,$currEl);
+
+
+
+            while($currEl==$nextEl) {
+            array_push($currentMatches,$currEl);
+               if(intval($k)<intval(count($arr))-1){
+                $k=$k+1;
+
+               }else {
+                   break;
+
+               }
+                $nextEl=$arr[$k];
+
+            if($currEl==$nextEl){
+                $i=$k;
+            }
+            }
+            array_push($arrMatches , $currentMatches);
+            $currentMatches=[];
+            break;
+      #  $i=$i+$k;
+        } else {
+
+            break;
+        }
+    }
+}
+
+//print_r($arrMatches);
+$previousValue=null;
+$max=[];
+foreach ($arrMatches as $findMax){
+  $currentValue = count($findMax);
+  if(empty($max)){
+    $max=$findMax;
+  }
+if($previousValue > $currentValue){
+    $max=$prevMax;
+}
+if($previousValue < $currentValue){
+    $max=$findMax;
+}
+    $previousValue = count($findMax);
+    $prevMax=$findMax;
+}
+echo implode(" ",$max);

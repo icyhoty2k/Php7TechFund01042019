cec154cc49ed43f1932814287ceb413ecb4cc96f
icyhoty2k <Code135790>
2019-03-31 16:34:55 +0300

    OfficePC

--- Lesson 29 Regular Expressions and strings/Task4L29CaesarCipher.php
+++ Lesson 29 Regular Expressions and strings/Task4L29CaesarCipher.php
@@ -1,1 +1,10 @@
 <?php
+
+$input = readline();
+
+for ($i=0; $i<strlen($input); $i++){
+   $x=ord($input[$i])+3;
+
+ echo   chr($x);
+
+}
--- Lesson14 MethodsFunctions/Task12L14Evennumbers.php
+++ Lesson14 MethodsFunctions/Task12L14Evennumbers.php
@@ -7,18 +7,32 @@
 
 </head>
 <body>
+<center>
 <form>
 N: <input type="text" name="num" />
     <input type="submit" />
 </form>
+</center>
 <?php
+
 if(isset($_GET['num'])){
+    echo '<center>
+    <table border="1">
+    <tr>';
+
     for ($i = 0; $i <= $_GET['num']; $i++) {
+
         if($i%2==0){
-            echo $i." ";
+            echo '<tr><td>'.$i.'</td></tr>';
         }
+
     }
+    ?><input type="text" name="num" value="<?php echo $_GET['num']; ?>"/><?php
 }
+echo '
+</tr>
+</table>
+</center>';
 ?>
 </body>
 </html>
